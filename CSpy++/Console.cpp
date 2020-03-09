33a9f52c6ea06f080dc787cbe8b5f4cfa63551eb
STY <jsjtsty@126.com>
2020-03-09 16:41:01 +0800

    2020/3/9 First Update

--- CSpy++/Console.cpp
+++ CSpy++/Console.cpp
@@ -1,7 +1,19 @@
 #include "general.h"
-#include <string>
-#include <map>
-using namespace std;
+#include "ArgParser.h"
+#include <cstdio>
+#include <iostream>
+#include "StringExt.h"
+#include "JsonExt.h"
+#include <openssl/crypto.h>
+#include <curl/curl.h>
+#include <zlib.h>
+using std::wcout;
+using std::endl;
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
 
 int __cdecl CommandLineMain()
 {
@@ -9,7 +21,31 @@ int __cdecl CommandLineMain()
 		return 0;
 	}
 
-	map<wstring, wstring> args;
+	//freopen("conout$", "wt+", stdout);
+	//freopen("conerr$", "wt+", stderr);
+	//freopen("conin$", "rt+", stdin);
 
-	
+	ArgParser parser;
+	if (!parser.parse(__argc, __wargv)) {
+		wcout << L"Invalid command." << endl;
+		return -1;
+	}
+
+	Argument arg{ L"v",L"-version" };
+	if (parser.hasArg(arg)) {
+		std::wstringstream ss;
+		printf("Fuck!\n");
+		wcout << L"CSpy++ " << CS_VERSION << L" jsoncpp/1.9.2 " << std::to_wstring(curl_version()) << endl;
+		wcout << L"Release Date: " << CS_VERSION_RELEASEDATE_YEAR << L"-" << CS_VERSION_RELEASEDATE_MONTH << L"-"
+			<< CS_VERSION_RELEASEDATE_DAY << endl;
+		MessageBox(NULL, ss.str().c_str(), L"Info", MB_ICONINFORMATION);
+		//WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), ss.str().c_str(), ss.str().size(), NULL, NULL);
+		return 1;
+	}
+
+	return 0;
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
--- CSpy++/Initialization.cpp
+++ CSpy++/Initialization.cpp
@@ -1,25 +1,18 @@
 #include "framework.h"
 #include "general.h"
 #include <string>
-#include "LogSupport.h"
+#include <locale>
 #include "LicenseReader.h"
 #include "RSAKey.h"
-#include "basic_form.h"
-using namespace std;
-using namespace log4cplus;
 
 void InitLogSystem();
 void InitConstants();
 
 int InitProgram()
 {
-	locale::global(locale("chs"));
+	std::locale::global(std::locale("chs"));
 	InitConstants();
 	InitLogSystem();
-	BasicForm* window = new BasicForm();
-	window->Create(NULL, BasicForm::kClassName.c_str(), WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX, 0);
-	window->CenterWindow();
-	window->ShowWindow();
 	return 0;
 }
 
@@ -31,9 +24,5 @@ int HandleCreateMessage(WPARAM wParam, LPARAM lParam)
 
 void InitLogSystem()
 {
-	SharedAppenderPtr append(new RollingFileAppender(L"cspypp.log"));
-	append->setName(L"defaultAppender");
-	append->setLayout(make_unique<PatternLayout>(L"[%p](%D{%Y-%m-%d %H:%M:%S.%q})[pid=%t, %l]: %m%n"));
-	logger = Logger::getInstance(L"defaultLogger");
-	logger.addAppender(append);
+
 }
